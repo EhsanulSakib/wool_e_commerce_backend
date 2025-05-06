90acf85e902735dcf0342011ce018ea6c3296f37
EhsanulSakib <ehsanulsakib.professional@gmail.com>
2025-05-06 15:05:34 +0600

    Add: MongoDB, Nodemailer, JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { jwtConfig } from './config/v1/jwt.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { mongodbConfig } from './config/v1/mongodb.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI', mongodbConfig.uri!),
+        dbName: process.env.DB_NAME,
+      }),
+      inject: [ConfigService],
+    }),
+    JwtModule.register(jwtConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
