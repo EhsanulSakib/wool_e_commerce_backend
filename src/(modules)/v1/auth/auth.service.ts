8258c63bb876a8f6ee68b62e0342a3437f1aecae
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2025-06-05 19:37:41 +0600

    Add: Logout, ForgetPassword, ResetPassword API

--- src/(modules)/v1/auth/auth.service.ts
+++ src/(modules)/v1/auth/auth.service.ts
@@ -19,6 +19,10 @@ import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { CreateUserLoginDto } from './dto/create-user-logn.dto';
 import { CreateUserVerifyDto } from './dto/create-user-verify.dto';
+import { CreateUserRefreshDto } from './dto/create-user-refresh.dto';
+import { VerifyRequest } from 'src/middlewares/v1/verify.middleware';
+import { CreateUserForgotPasswordDto } from './dto/create-user-forgot-pass.dto';
+import { CreateUserResetPasswordDto } from './dto/create-user-reset-pass.dto';
 
 @Injectable()
 export class AuthService {
@@ -176,11 +180,11 @@ export class AuthService {
       const { token } = createUserVerifyDto || {};
 
       const decode = await this.jwtService.verifyAsync(token, {
-          secret: process.env.JWT_SECRET,
-      })
+        secret: process.env.JWT_SECRET,
+      });
 
       if (!decode) {
-          throw new BadRequestException('Unauthorized Access');
+        throw new BadRequestException('Unauthorized Access');
       }
 
       const { cid } = decode;
@@ -189,29 +193,216 @@ export class AuthService {
       const isExpired = decode.exp < currentDate;
 
       if (isExpired) {
-          throw new BadRequestException('Token expired');
+        throw new BadRequestException('Token expired');
       }
 
       const user = await this.userModel.findOne({ cid }).exec();
       if (!user) throw new NotFoundException('User not found');
 
       if (user.status === UserStatus.Active)
-          throw new BadRequestException('User is already active');
-    
+        throw new BadRequestException('User is already active');
+
       if (user.status === UserStatus.Banned) {
-          throw new BadRequestException('User is banned');
+        throw new BadRequestException('User is banned');
       }
 
       if (user.status === UserStatus.Inactive) {
-          const updatedUser = await this.userModel
-              .findOneAndUpdate({ cid }, { $set: { status: UserStatus.Active } }, { new: true })
-              .exec();
-          if (!updatedUser) throw new NotFoundException('User not found');
-          return updatedUser;
+        const updatedUser = await this.userModel
+          .findOneAndUpdate(
+            { cid },
+            { $set: { status: UserStatus.Active } },
+            { new: true },
+          )
+          .exec();
+        if (!updatedUser) throw new NotFoundException('User not found');
+        return updatedUser;
+      }
+    } catch (e) {
+      console.log(e.message);
+      throw new InternalServerErrorException('Unable to activate the user', {
+        cause: new Error(),
+        description: e.message,
+      });
+    }
+  }
+
+  async refresh(createUserRefreshDto: CreateUserRefreshDto) {
+    try {
+      const { refreshToken } = createUserRefreshDto;
+
+      const decode = await this.jwtService.verifyAsync(refreshToken, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      if (!decode) {
+        throw new BadRequestException('Unauthorized Access');
       }
 
+      const refreshTokenFromCache = await this.cacheManager.get(
+        `${decode.cid}_refresh_token`,
+      );
+
+      if (!refreshTokenFromCache) {
+        throw new BadRequestException('Unauthorized Access');
+      }
+
+      if (refreshToken !== refreshTokenFromCache) {
+        throw new BadRequestException('Unauthorized Access');
+      }
+
+      const cachedTokenDecoded = await this.jwtService.verify(
+        refreshTokenFromCache,
+        {
+          secret: process.env.JWT_SECRET,
+        },
+      )
+
+      const currentDate = Math.floor(Date.now() / 1000);
+      const isExpired = cachedTokenDecoded.exp < currentDate;
+
+      if(!cachedTokenDecoded || isExpired) {
+        throw new BadRequestException('Unauthorized Access');
+      }
+
+      const payload: Session = {
+        cid: cachedTokenDecoded.cid,
+        email: cachedTokenDecoded.email,
+        userName: cachedTokenDecoded.userName,
+      };
+
+      const { accessToken, refreshToken: newRefreshToken } =
+        this.createAccessAndRefreshToken(payload);
+
+      await this.cacheManager.set(
+        `${cachedTokenDecoded.cid}_refresh_token`,
+        newRefreshToken,
+        6048000
+      )
+
+      return {
+        accessToken,
+        refreshToken: newRefreshToken,
+      }
     } catch (e) {
-      throw new BadRequestException('Invalid token');
+      throw new InternalServerErrorException('Unable to refresh token', {
+        cause: new Error(),
+        description: e.message,
+      });
+    }
+  }
+
+  async logout(req: VerifyRequest){
+    try{
+      const user: Session = req.user;
+
+      await this.cacheManager.del(`${user.cid}_access_token`);
+      await this.cacheManager.del(`${user.cid}_refresh_token`);
+
+      return {
+        success: true,
+        message: 'User logged out successfully',
+      };
+    }
+    catch (e) {
+      throw new InternalServerErrorException('Unable to logout user', {
+        cause: new Error(),
+        description: e.message,
+      });
+    }
+  }
+
+  async forgotPassword(createUserForgotPasswordDto: CreateUserForgotPasswordDto) {
+    try{
+      const {email} = createUserForgotPasswordDto
+
+      const user = await this.userModel.findOne({ email }).exec();
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      if (user.status !== UserStatus.Active) {
+        throw new BadRequestException('User is not active');
+      }
+
+      const payload = {
+        cid: user.cid.toString(),
+        email: user.email,
+      }
+
+      const token = this.jwtService.sign(payload, {
+        secret: process.env.JWT_SECRET,
+        expiresIn: '1h',
+      });
+
+      await this.cacheManager.set(
+        `${user.cid.toString()}_forgot_password_token`,
+        token,
+        this.hourExpire,
+      );
+
+      const resetPasswordLink = `${process.env.CLIENT_URL}/reset-password?token=${token}`
+
+      await this.sendMailUtil.sendMail(
+        `Please use the following link to reset your password: ${resetPasswordLink}`,
+        user.email,
+        'Reset Password (Wool)',
+      );
+
+      return {
+        status: true,
+        message: 'Password reset link sent to your email. Please check your inbox.',
+      };
+
+    }
+    catch(e){
+      throw new InternalServerErrorException('Unable to process forgot password request', {
+        cause: new Error(),
+        description: e.message,
+      });
+    }
+  }
+
+  async resetPassword(createUserResetPasswordDto: CreateUserResetPasswordDto) {
+    try {
+      const { token, newPassword } = createUserResetPasswordDto;
+
+      const decode = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      const tokenFromCache = await this.cacheManager.get(
+        `${decode.cid}_reset_password_token`,
+      );
+
+      if (!tokenFromCache || token !== tokenFromCache) {
+        throw new BadRequestException('Invalid or expired token');
+      }
+
+      const user = await this.userModel.findOne({ cid: decode.cid }).exec();
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      } 
+
+      // Hash the new password
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      
+      // Update the user's password
+      user.password = hashedPassword;
+      await user.save();
+
+      // Remove the token from cache
+      await this.cacheManager.del(`${decode.cid}_reset_password_token`);
+
+      return {
+        status: true,
+        message: 'Password reset successfully!',
+      }
+    } catch (e) {
+      throw new InternalServerErrorException(
+        e.message || 'Failed to reset password',
+      );
     }
   }
 }
